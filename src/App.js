8c49006c4faf7197204db9644c1b252829375635
dapierce <pierceda@gmail.com>
2018-07-19 13:25:42 -0700

    using react-reflex to enable draggable side panel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import {
+  ReflexContainer,
+  ReflexSplitter,
+  ReflexElement
+} from 'react-reflex';
+
+import 'react-reflex/styles.css'
+
 import './App.css';
 import Panel from './Panel.js';
 import Content from './Content.js';
@@ -7,8 +15,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Panel />
-        <Content />
+        <ReflexContainer orientation="vertical">
+
+          <ReflexElement className="left-pane" size="250" minSize="200" maxSize="600">
+            <Panel />
+          </ReflexElement>
+
+          <ReflexSplitter propagate={true}/>
+
+          <ReflexElement className="right-pane">
+            <Content />
+          </ReflexElement>
+
+        </ReflexContainer>
       </div>
     );
   }
--- src/Panel.js
+++ src/Panel.js
@@ -9,7 +9,7 @@ class Panel extends Component {
         <h1 className="header">Noteticker</h1>
         <Search />
         <NotebookList/>
-        <button className="button-new" title="Create New Notebook">+ New Notebook</button>
+        <button className="button-new" title="Create a New Notebook">+ New Notebook</button>
         <button className="button-prefs" title="Preferences">
           <span role="img" aria-label="Wrench">🔧</span>
         </button>
